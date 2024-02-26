3af2641b441279d13ab9f35152e15c12d3d11212
Rubenrrb <76271283+Rubenrrb@users.noreply.github.com>
2024-02-26 15:39:18 +0900

    added function to delete products

---
+++ sick-fits/frontend/components/DeleteProduct.js
@@ -0,0 +1,37 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+function update(cache, payload) {
+  console.log(payload);
+  cache.evict(cache.identify(payload.data.deleteProduct));
+}
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProdct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
+    variables: { id },
+    update,
+  });
+
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        if (confirm('Are you sure you want to delete this item?')) {
+          deleteProdct().catch((err) => alert(err.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}
