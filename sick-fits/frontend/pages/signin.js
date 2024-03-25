8551c1fd154883f7257021b9113a7f301d0e931c
Rubenrrb <76271283+Rubenrrb@users.noreply.github.com>
2024-03-25 11:12:52 +0900

    added sign in/out

---
+++ sick-fits/frontend/pages/signin.js
@@ -0,0 +1,10 @@
+import SignIn from '../components/SignIn';
+
+/* eslint-disable react/display-name */
+export default function SignInPage() {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+}
